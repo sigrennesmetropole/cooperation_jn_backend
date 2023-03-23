3c9774ed926c5bfa90672669ee02666dceae6f47
Toni GARCIA <toni.garcia.yt@gmail.com>
2023-03-23 16:53:09 +0100

    api enedis: format result

---
+++ src/model/autocalsol.model.ts
@@ -0,0 +1,20 @@
+export type AutocalsolType = {
+    idStudy: number,
+    puissanceCreteTotale: number,
+    resultBatiments: null,
+    resultConso: {
+        tabProdMonth: number[],
+        tabConsoMonth: number[],
+        prodTotale: number [][],
+        consoPetit: number [][],
+        energieInjectee: number,
+        energieAutoconsommee: number,
+        energieSoutiree: number
+    },
+    resultAutoConso: {
+        tabProdJournaliere: Record<string, number>,
+        tabConsoJournaliere: Record<string, number>,
+        tabInjectionJournaliere: Record<string, number>
+    }
+  }
+  
--- src/services/api-autocalsol.ts
+++ src/services/api-autocalsol.ts
@@ -1,5 +1,5 @@
 import axios, { Method } from 'axios';
-import qs from 'qs';
+import {type AutocalsolType} from '../model/autocalsol.model';
 
 function getUrlFromEnv(){
     return process.env.AUTOCALSOL_URL
@@ -13,9 +13,47 @@ function getTokenFromEnv(){
     return process.env.AUTOCALSOL_TOKEN
 }
 
-function convertTimestampToDate(timestamp: number){
-    const date = new Date(timestamp * 1000)
-    return date.toISOString().split('T')[0]
+function convertTimestamp(timestamp: number) {
+    // Create a new Date object using the timestamp
+    const date = new Date(timestamp);
+  
+    // Format the date using the built-in methods
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    const seconds = date.getSeconds().toString().padStart(2, '0');
+  
+    // Return the formatted date string
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+}
+
+function formatComputeData(compute: AutocalsolType){
+    const prodByMonth = compute.resultConso.tabProdMonth
+    const consoByMonth = compute.resultConso.tabConsoMonth
+    const prodByHour = compute.resultConso.prodTotale.map(item => {
+        return [
+            convertTimestamp(item[0]),
+            item[1]
+        ]
+    })
+    const consoByHour = compute.resultConso.consoPetit.map(item => {
+        return [
+            convertTimestamp(item[0]),
+            item[1]
+        ]
+    })
+    const consoAnnualInjected = compute.resultConso.energieInjectee / 1000 // Wh to kwH
+    const consoAnnualAutoConsumed = compute.resultConso.energieAutoconsommee / 1000 // Wh to kwH
+    return {
+        prodByMonth: prodByMonth,
+        consoByMonth: consoByMonth,
+        prodByHour: prodByHour,
+        consoByHour: consoByHour,
+        consoAnnualInjected: consoAnnualInjected,
+        consoAnnualAutoConsumed: consoAnnualAutoConsumed
+    }
 }
 
 export async function getComputeData(
@@ -85,7 +123,8 @@ export async function getComputeData(
     };
     try {
         const response = await axios(config)
-        return response.data
+        const data = response.data
+        return formatComputeData(data)
     } catch (error) {
         throw new Error(`HTTP error`);
     }
