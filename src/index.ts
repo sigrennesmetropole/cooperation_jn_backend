e464f0e49dd1dba75b3ffef98137c45cd6bcec40
Yohann Marchat <yohann.marchat@camptocamp.com>
2023-04-18 16:17:17 +0200

    catch async error

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import express, { Express, Request, Response } from 'express';
+import express, {Express, Request, Response} from 'express';
 import dotenv from 'dotenv';
-import session, { SessionData, SessionOptions } from 'express-session';
-import { getUrlUserAuthorization, getAnnualConsumption } from './services/api-enedis-dataconnect';
-import { getTotalDistrictDatas } from './services/api-enedis-district';
-import { getComputeData } from './services/api-autocalsol';
-import { MySessionData } from './interface/MySessionData';
+import session, {SessionData, SessionOptions} from 'express-session';
+import {getUrlUserAuthorization, getAnnualConsumption} from './services/api-enedis-dataconnect';
+import {getTotalDistrictDatas} from './services/api-enedis-district';
+import {getComputeData} from './services/api-autocalsol';
+import {MySessionData} from './interface/MySessionData';
+
+const asyncHandler = require('express-async-handler')
 
 // Load environment variables from .env file
 dotenv.config();
@@ -12,90 +14,91 @@ dotenv.config();
 const app: Express = express();
 
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*'); // Set to the request's origin or use a specific domain
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', 'true'); // Allow credentials (cookies) to be sent
-  next();
+    res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*'); // Set to the request's origin or use a specific domain
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', 'true'); // Allow credentials (cookies) to be sent
+    next();
 });
 
 app.use(session({
-  secret: 'secret-key',
-  resave: false,
-  saveUninitialized: false,
+    secret: 'secret-key',
+    resave: false,
+    saveUninitialized: false,
 }));
 
 app.use(express.json());
 
 // ROUTES API ENEDIS USER
 app.get('/api/enedis/user/url-authorization', (req: Request & { session: MySessionData }, res: Response) => {
-  res.json({ url: getUrlUserAuthorization(req) });
+    res.json({url: getUrlUserAuthorization(req)});
 });
 
 app.get('/api/enedis/user/prm', (req: Request & { session: MySessionData }, res: Response) => {
-  const prm = req.session.prm
-  res.json({ prm: prm });
+    const prm = req.session.prm
+    res.json({prm: prm});
 });
 
 app.post('/api/enedis/user/prm', (req: Request & { session: MySessionData }, res: Response) => {
-  const prm = req.body.prm as string | undefined;
-  if(prm === undefined){
-    res.status(400).json({ error: 'Missing required body parameters' });
-  }
-  req.session.prm = prm
-  req.session.save()
-  res.json({ prm: prm });
+    const prm = req.body.prm as string | undefined;
+    if (prm === undefined) {
+        res.status(400).json({error: 'Missing required body parameters'});
+    }
+    req.session.prm = prm
+    req.session.save()
+    res.json({prm: prm});
 });
 
 app.get(
-  '/api/enedis/user/annual-consumption', 
-  async (req: Request & { session: MySessionData }, res: Response) => {
-    const annual_consumption = await getAnnualConsumption(req) 
-    res.json({ consumption: annual_consumption});
-  }
-);
+    '/api/enedis/user/annual-consumption',
+    asyncHandler(async (req: Request & { session: MySessionData }, res: Response) => {
+            const annual_consumption = await getAnnualConsumption(req)
+            res.json({consumption: annual_consumption});
+        }
+    ));
 
 // ROUTES API ENEDIS DISTRICT
-app.get('/api/enedis/district/:codeIris', async (req: Request & { session: MySessionData }, res: Response) => {
-  const districtDatas = await getTotalDistrictDatas(req.params.codeIris)
-  res.json( districtDatas );
-});
+app.get('/api/enedis/district/:codeIris',
+    asyncHandler(async (req: Request & { session: MySessionData }, res: Response) => {
+        const districtDatas = await getTotalDistrictDatas(req.params.codeIris)
+        res.json(districtDatas);
+    }));
 
 // ROUTES API AUTOCALSOL
 app.get(
-  '/api/autocalsol/data-compute',
-  async (req: Request & { session: MySessionData }, res: Response) => {
-    const latitude = req.query.latitude as string | undefined;
-    const longitude = req.query.longitude as string | undefined;
-    const slope = req.query.slope as number | undefined;
-    const azimuth = req.query.azimuth as number | undefined;
-    const annual_consumption = req.query.annual_consumption as number | undefined;
-    const peak_power = req.query.peak_power as number | undefined;
+    '/api/autocalsol/data-compute',
+    asyncHandler(async (req: Request & { session: MySessionData }, res: Response) => {
+        const latitude = req.query.latitude as string | undefined;
+        const longitude = req.query.longitude as string | undefined;
+        const slope = req.query.slope as number | undefined;
+        const azimuth = req.query.azimuth as number | undefined;
+        const annual_consumption = req.query.annual_consumption as number | undefined;
+        const peak_power = req.query.peak_power as number | undefined;
 
-    if (
-      latitude !== undefined &&
-      longitude !== undefined &&
-      slope !== undefined &&
-      azimuth !== undefined &&
-      annual_consumption !== undefined &&
-      peak_power !== undefined
-    ) {
-      const compute = await getComputeData(
-        latitude,
-        longitude,
-        slope,
-        azimuth,
-        annual_consumption,
-        peak_power
-      );
-      res.json({ compute: compute });
-    } else {
-      res.status(400).json({ error: 'Missing required query parameters' });
-    }
-  }
+        if (
+            latitude !== undefined &&
+            longitude !== undefined &&
+            slope !== undefined &&
+            azimuth !== undefined &&
+            annual_consumption !== undefined &&
+            peak_power !== undefined
+        ) {
+            const compute = await getComputeData(
+                latitude,
+                longitude,
+                slope,
+                azimuth,
+                annual_consumption,
+                peak_power
+            );
+            res.json({compute: compute});
+        } else {
+            res.status(400).json({error: 'Missing required query parameters'});
+        }
+    })
 );
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Running on port ${port}`);
+    console.log(`Running on port ${port}`);
 });
--- src/services/api-enedis-dataconnect.ts
+++ src/services/api-enedis-dataconnect.ts
@@ -1,99 +1,99 @@
-import { Request } from 'express';
-import { MySessionData } from '../interface/MySessionData';
-import axios, { Method } from 'axios';
-import qs from 'qs';
+import { type Request } from 'express'
+import { type MySessionData } from '../interface/MySessionData'
+import axios, { type Method } from 'axios'
+import qs from 'qs'
 
-export function getUrlUserAuthorization(req: Request & { session: MySessionData }) {
-    req.session.state = (Math.random() + 1).toString(36).substring(7)
-    req.session.save()
-    const url =
+export function getUrlUserAuthorization (req: Request & { session: MySessionData }) {
+  req.session.state = (Math.random() + 1).toString(36).substring(7)
+  req.session.save()
+  const url =
         'https://mon-compte-particulier.enedis.fr/dataconnect/v1/oauth2/authorize' +
         '?' +
         `client_id=${process.env.ENEDIS_CLIENT_ID}` +
         `&state=${req.session.state}` +
         `&duration=${process.env.ENEDIS_CLIENT_DURATION}` + // duration est la durée du consentement que vous souhaitez obtenir : cette durée est à renseigner au format ISO 8601 (exemple : « P6M » pour une durée de 6 mois),
         '&response_type=code'
-    return url
+  return url
 }
 
-function getUrlFromEnv(){
-    if(process.env.ENV === 'dev'){
-        return process.env.ENEDIS_URL_SANDBOX
-    } else {
-        return process.env.ENEDIS_URL_PRODUCTION
+function getUrlFromEnv () {
+  if (process.env.ENV === 'dev') {
+    return process.env.ENEDIS_URL_SANDBOX
+  } else {
+    return process.env.ENEDIS_URL_PRODUCTION
+  }
+}
+
+async function getUserAccessToken () {
+  const data = qs.stringify({
+    grant_type: 'client_credentials',
+    client_id: process.env.ENEDIS_CLIENT_ID,
+    client_secret: process.env.ENEDIS_CLIENT_SECRET
+  })
+  const config = {
+    method: 'post' as Method,
+    url: getUrlFromEnv() + '/oauth2/v3/token',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    data
+  }
+
+  const response = await axios(config)
+  return response.data
+}
+
+async function getDailyConsumption (access_token: string, prm: string, start: string, end: string) {
+  const config = {
+    method: 'get' as Method,
+    url: `${getUrlFromEnv()}/metering_data_dc/v5/daily_consumption?start=${start}&end=${end}&usage_point_id=${prm}`,
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+      accept: 'application/json'
     }
+  }
+  const response = await axios(config)
+  return response.data
 }
 
-async function getUserAccessToken() {
-    var data = qs.stringify({
-        'grant_type': 'client_credentials',
-        'client_id':  process.env.ENEDIS_CLIENT_ID,
-        'client_secret': process.env.ENEDIS_CLIENT_SECRET 
-    });
-    var config = {
-        method: 'post' as Method,
-        url: getUrlFromEnv() + '/oauth2/v3/token',
-        headers: { 
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        data : data
-    };
-
-    const response = await axios(config)
-    return response.data
+function calculateAnnualConsumption (interval_reading: [{ value: string, date: string }]) {
+  let annualConsumption = 0
+  interval_reading.forEach((item) => {
+    annualConsumption += parseInt(item.value)
+  })
+  return annualConsumption
 }
 
-async function getDailyConsumption(access_token: string, prm: string, start: string, end: string) {
-    var config = {
-        method: 'get' as Method,
-        url: `${getUrlFromEnv()}/metering_data_dc/v5/daily_consumption?start=${start}&end=${end}&usage_point_id=${prm}`,
-        headers: { 
-            'Authorization': `Bearer ${access_token}`,
-            'accept': `application/json`
-        }
-    };
-    const response = await axios(config)
-    return response.data
-}
-
-function calculateAnnualConsumption(interval_reading: [{value: string, date: string}]){
-    let annualConsumption = 0
-    interval_reading.forEach((item) => {
-        annualConsumption += parseInt(item.value)
-    })
-    return annualConsumption
-}
-
-export async function getAnnualConsumption(req: Request & { session: MySessionData }){
-    const prm = req.session.prm
-    if(prm === undefined){
-        throw new Error(`PRM undefined`);
+export async function getAnnualConsumption (req: Request & { session: MySessionData }) {
+  const prm = req.session.prm
+  if (prm === undefined) {
+    throw new Error('PRM undefined')
+  }
+  try {
+    const token_response = await getUserAccessToken()
+    const access_token = token_response.access_token
+    const start = new Date().getFullYear() - 1 + '-01-01'
+    const end = new Date().getFullYear() - 1 + '-12-31'
+    const consumption_response = await getDailyConsumption(access_token, prm, start, end)
+    if (
+      consumption_response.meter_reading === undefined ||
+            consumption_response.meter_reading.interval_reading === undefined
+    ) {
+      throw new Error('consumption_response.meter_reading.interval_reading undefined')
     }
-    try {
-        const token_response = await getUserAccessToken()
-        const access_token = token_response.access_token
-        const start = new Date().getFullYear() - 1 + '-01-01'
-        const end = new Date().getFullYear() - 1 + '-12-31'
-        const consumption_response = await getDailyConsumption(access_token, prm, start, end)
-        if(
-            consumption_response.meter_reading === undefined
-            || consumption_response.meter_reading.interval_reading === undefined
-        ){
-            throw new Error(`consumption_response.meter_reading.interval_reading undefined`);
-        }
-        const interval_reading = consumption_response.meter_reading.interval_reading
-        const annualConsumption = calculateAnnualConsumption(interval_reading)
-        if(
-            consumption_response.meter_reading.reading_type === undefined
-        ){
-            throw new Error(`consumption_response.meter_reading.reading_type undefined`);
-        }
-        const reading_type = consumption_response.meter_reading.reading_type
-        return {
-            annual_consumption: annualConsumption,
-            reading_type: reading_type
-        }
-    } catch (error) {
-        throw new Error(`HTTP error`);
-    }  
+    const interval_reading = consumption_response.meter_reading.interval_reading
+    const annualConsumption = calculateAnnualConsumption(interval_reading)
+    if (
+      consumption_response.meter_reading.reading_type === undefined
+    ) {
+      throw new Error('consumption_response.meter_reading.reading_type undefined')
+    }
+    const reading_type = consumption_response.meter_reading.reading_type
+    return {
+      annual_consumption: annualConsumption,
+      reading_type
+    }
+  } catch (error) {
+    throw new Error('HTTP error')
+  }
 }
