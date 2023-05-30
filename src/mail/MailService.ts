75f304af267fe1a0a54a22ba6a82d7cf7b76c54f
Toni GARCIA <toni.garcia.yt@gmail.com>
2023-05-30 17:29:38 +0200

    test send email

--- src/mail/MailService.ts
+++ src/mail/MailService.ts
@@ -1,21 +1,25 @@
 const nodemailer = require('nodemailer')
 
 export function sendEmailPdf (
-  pdfBuffer: Buffer,
+  pdfBuffer: Buffer | null,
   email: string
 ) {
   // EMAIL PDF
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
-    secure: false, // true for 465, false for other ports
+    secure: true, // use TLS
     auth: {
       user: process.env.EMAIL_USER, // generated ethereal user
       pass: process.env.EMAIL_PASSWORD // generated ethereal password
+    },
+    tls: {
+      // do not fail on invalid certs
+      rejectUnauthorized: false 
     }
   })
 
-  const mailOptions = {
+  const mailOptions: any = {
     from: 'testpdf@gmail.com',
     to: email,
     subject: 'Votre simulation d\'installation photovoltaïque',
@@ -23,8 +27,12 @@ export function sendEmailPdf (
             Bonjour,
             Veuillez trouver ci joint votre simulation d'installation photovoltaïque.
             Cordialement
-        `,
-    attachments: [{
+        `
+  }
+
+  // If pdfBuffer is provided, include it in the attachments
+  if (pdfBuffer) {
+    mailOptions.attachments = [{
       filename: 'attachment.pdf',
       content: pdfBuffer
     }]
