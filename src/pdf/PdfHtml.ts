76f84170378dfbe3e51a3b1fad225702c71871dd
Ismail Sunni <imajimatika@gmail.com>
2023-08-25 17:41:57 +0700

    Attempting to fix linter.

--- src/pdf/PdfHtml.ts
+++ src/pdf/PdfHtml.ts
@@ -1,17 +1,20 @@
-import { generateChartImg } from './chart/PdfChart'
-import { getPage1 } from './PdfPage1'
-import { getPage2 } from './PdfPage2'
-import { getPage3 } from './PdfPage3'
-import { getPage4 } from './PdfPage4'
-import { getPage5 } from './PdfPage5'
-import { getPage6 } from './PdfPage6'
-import { getPdfStyle } from './PdfStyle'
-import { type RoofSurfaceModel, type AutocalsolResult as AutocalsolResultType } from './type/type'
-import { getConfigFromKey } from '../config/configService'
+import { generateChartImg } from "./chart/PdfChart";
+import { getPage1 } from "./PdfPage1";
+import { getPage2 } from "./PdfPage2";
+import { getPage3 } from "./PdfPage3";
+import { getPage4 } from "./PdfPage4";
+import { getPage5 } from "./PdfPage5";
+import { getPage6 } from "./PdfPage6";
+import { getPdfStyle } from "./PdfStyle";
+import {
+  type RoofSurfaceModel,
+  type AutocalsolResult as AutocalsolResultType,
+} from "./type/type";
+import { getConfigFromKey } from "../config/configService";
 
-const fs = require('fs')
+const fs = require("fs");
 
-export async function getPdfHtml (
+export async function getPdfHtml(
   data_autocalsol: AutocalsolResultType,
   selectedRoof: RoofSurfaceModel,
   address: string,
@@ -22,9 +25,10 @@ export async function getPdfHtml (
   districtProduction: number,
   imgRoofBase64: string
 ) {
-  const currentSurface = currentNumSolarPanel * getConfigFromKey('solar_panel.solar_panel_surface')
+  const currentSurface =
+    currentNumSolarPanel * getConfigFromKey("solar_panel.solar_panel_surface");
 
-  const chartImageBase64 = await generateChartImg(data_autocalsol)
+  const chartImageBase64 = await generateChartImg(data_autocalsol);
 
   const html = `
         <html>
@@ -34,27 +38,25 @@ export async function getPdfHtml (
             <body>
                 ${getPage1()}
                 ${getPage2(
-                    address,
-                    selectedRoof,
-                    annualConsumption,
-                    currentNumSolarPanel,
-                    currentPower,
-                    currentSurface,
-                    imgRoofBase64
+                  address,
+                  selectedRoof,
+                  annualConsumption,
+                  currentNumSolarPanel,
+                  currentPower,
+                  currentSurface,
+                  imgRoofBase64
                 )}
                 ${getPage3(
-                    data_autocalsol,
-                    districtNumberInstallations,
-                    districtProduction
+                  data_autocalsol,
+                  districtNumberInstallations,
+                  districtProduction
                 )}
-                ${getPage4(
-                    chartImageBase64
-                )}
+                ${getPage4(chartImageBase64)}
                 ${getPage5()}
                 ${getPage6()}
 
             </body>
         </html>
-    `
-  return html
+    `;
+  return html;
 }
