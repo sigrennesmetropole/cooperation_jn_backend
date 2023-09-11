b40e3c3347041275413774342c4c2deebfad7f08
Ismail Sunni <imajimatika@gmail.com>
2023-09-11 14:51:07 +0700

    Fix rounding in PDF.

--- src/pdf/components/ProductionInformationText.ts
+++ src/pdf/components/ProductionInformationText.ts
@@ -5,18 +5,18 @@ export function getProductionInformationText (
   currentNumSolarPanel: number,
   currentPower: number,
   currentSurface: number
-) {
+): string {
   return `
         <div class="flex-column box-white" style="margin-top:20px;">
             <div class="flex-row items-center gap-4" style="margin-top: 10px;">
                 ${getInstallationImg()}
                 <span class="font-bold text-2xl" style="margin-top: 25px;">
                     Votre installation
-                </span> 
+                </span>
             </div>
 
             <div
-                class="flex-column gap-4" 
+                class="flex-column gap-4"
                 style="padding: 16px; border-radius: 0.75rem; background-color: rgb(248,250,252); border-color: rgb(241,245,249); border-width: 1px; border-style: solid;"
             >
                 <div class="flex-row p-0" style="justify-content:center; align-items:center;">
@@ -34,18 +34,18 @@ export function getProductionInformationText (
                         soit ${currentPower} kWc de puissance installée
                     </p>
                 </div>
-                                
+
 
                 <div style="width: 100%; border: 1px solid #D4D4D4;"></div>
 
 
-                <div 
-                    class="flex-column" 
+                <div
+                    class="flex-column"
                 >
                     <span class="font-dm-sans font-normal text-sm">
                         Surface de l’installation photovoltaïque
                     </span>
-                    <span class="font-bold text-base"> ${currentSurface}m&sup2; </span>
+                    <span class="font-bold text-base"> ${currentSurface.toFixed(2)}m&sup2; </span>
                 </div>
             </div>
         </div>
