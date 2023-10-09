4a92eb301b05f0b9e98ab31ee9a8ea8ba0f9e5fa
Yohann Marchat <yohann.marchat@camptocamp.com>
2023-10-09 15:11:26 +0200

    fix: pdf p5 waiting for promise

--- src/pdf/components/LinkWithIconArrow.ts
+++ src/pdf/components/LinkWithIconArrow.ts
@@ -1,6 +1,6 @@
 import { getExpandImg } from '../assets/expandImg'
 
-export function getLinkWithIconArrow(link: string) {
+export function getLinkWithIconArrow(link: Promise<any>) {
   return `
         <a
             href="${link}"
