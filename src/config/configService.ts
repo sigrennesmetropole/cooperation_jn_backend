7a2c054b03b3d2198cc8e59aad954a6ee06e8097
Ismail Sunni <imajimatika@gmail.com>
2023-12-20 12:40:03 +0700

    Print config for running test.

--- src/config/configService.ts
+++ src/config/configService.ts
@@ -14,6 +14,7 @@ export async function getConfig() {
   }
   if (process.env.CONF === 'local') {
     console.log('Local setup: use local configuration')
+    console.log(configuration)
     return configuration
   }
   console.log('Server setup: use remote configuration')
